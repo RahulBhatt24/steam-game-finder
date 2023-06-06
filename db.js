d693728b1f898e44720099aa92da83ed026cdd79
RahulBhatt24 <rbhatt06@gmail.com>
2023-06-06 15:42:57 -0400

    steam game finder

---
+++ db.js
@@ -0,0 +1,292 @@
+const express = require('express');
+const { createPool } = require('mysql');
+const bcrypt = require('bcrypt');
+const session = require('express-session');
+
+const app = express();
+
+const pool = createPool({
+  host: '34.134.88.232',
+  port: 3306,
+  user: 'root',
+  password: 'NRN',
+  database: 'GameFinder',
+});
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static('public'));
+app.use(
+  session({
+    secret: 'your-secret-key',
+    resave: false,
+    saveUninitialized: false,
+  })
+);
+
+let cachedData = null;
+let cacheTimestamp = null;
+const cacheDuration = 48 * 60 * 60 * 1000; // 48h in milliseconds
+
+app.get('/data', (req, res) => {
+  const currentTime = Date.now();
+
+  if (cachedData && cacheTimestamp && currentTime - cacheTimestamp < cacheDuration) {
+    res.json(cachedData);
+    return;
+  }
+
+  pool.query(
+    'SELECT g.QueryName, AVG(r.review_score) AS avg_review_score FROM Games g JOIN Reviews r ON g.QueryName = r.app_name GROUP BY g.QueryID, g.QueryName HAVING AVG(r.review_score) >= 0.75 LIMIT 15;',
+    // 'SELECT * from likes LIMIT 15;',
+    (err, results) => {
+      if (err) {
+        console.error('Error executing query:', err);
+        res.status(500).send('Error executing query');
+        return;
+      }
+
+      cachedData = results;
+      cacheTimestamp = currentTime;
+      res.json(results);
+    }
+  );
+});
+
+app.get('/gb-fan-fave', (req, res) => {
+  pool.query(
+    'SELECT * FROM gf_fan_fave;',
+    (err, results) => {
+      if (err) {
+        console.error('Error executing query:', err);
+        res.status(500).send('Error executing query');
+        return;
+      }
+      res.json(results);
+    }
+  );
+});
+
+app.get('/data2', (req, res) => {
+  pool.query(
+    'SELECT DISTINCT pu.Name, AVG(pu.Hours) AS avg_hours, pc.PlatformWindows FROM Purchases pu JOIN PC pc ON pu.Name = pc.QueryName WHERE pc.PlatformWindows = 1 GROUP BY pu.Name ORDER BY AVG(pu.Hours) DESC LIMIT 15;',
+    (err, results) => {
+      if (err) {
+        console.error('Error executing query:', err);
+        res.status(500).send('Error executing query');
+        return;
+      }
+      res.json(results);
+    }
+  );
+});
+
+app.get('/best-value-games', (req, res) => {
+  pool.query('CALL bestVal();', (err, results) => {
+    if (err) {
+      console.error('Error executing stored procedure:', err);
+      res.status(500).send('Error executing stored procedure');
+      return;
+    }
+    res.json(results[0]);
+  });
+});
+
+
+// Signup endpoint
+app.post('/signup', async (req, res) => {
+  const { email, password } = req.body;
+  const hashedPassword = await bcrypt.hash(password, 10);
+
+  pool.query(
+    'INSERT INTO users (email, password_hash) VALUES (?, ?)',
+    [email, hashedPassword],
+    (err, results) => {
+      if (err) {
+        console.error('Error inserting user:', err);
+        res.status(500).send('Error signing up');
+        return;
+      }
+      res.send('Signup successful');
+    }
+  );
+});
+
+app.get('/data3', (req, res) => {
+  pool.query(
+    'SELECT * FROM users',
+    (err, results) => {
+      if (err) {
+        console.error('Error executing query:', err);
+        res.status(500).send('Error executing query');
+        return;
+      }
+      res.json(results);
+    }
+  );
+});
+
+app.get('/search-games', (req, res) => {
+  const keyword = req.query.keyword;
+
+  pool.query(
+    "SELECT QueryName FROM Games WHERE QueryName LIKE CONCAT('%', ?, '%') GROUP BY QueryName;",
+    [keyword],
+    (err, results) => {
+        if (err) {
+            console.error('Error executing query:', err);
+            res.status(500).send('Error executing query');
+            return;
+        }
+        res.json(results);
+    }
+  );
+
+  // pool.query(
+  //     "SELECT Name FROM Purchases WHERE Name LIKE CONCAT('%', ?, '%') GROUP BY Name;",
+  //     [keyword],
+  //     (err, results) => {
+  //         if (err) {
+  //             console.error('Error executing query:', err);
+  //             res.status(500).send('Error executing query');
+  //             return;
+  //         }
+  //         res.json(results);
+  //     }
+  // );
+});
+
+app.post('/toggle-favorite', async (req, res) => {
+  const { game_id } = req.body;
+  const user_id = req.session.user.id;
+
+  pool.query(
+    'SELECT * FROM likes WHERE user_id = ? AND game_id = ?;',
+    [user_id, game_id],
+    (err, results) => {
+      if (err) {
+        console.error('Error checking favorite:', err);
+        res.status(500).send('Error checking favorite');
+        return;
+      }
+
+      if (results.length > 0) {
+        // Remove the favorite
+        pool.query(
+          'DELETE FROM likes WHERE user_id = ? AND game_id = ? ;',
+          [user_id, game_id],
+          (err, results) => {
+            if (err) {
+              console.error('Error removing favorite:', err);
+              res.status(500).send('Error removing favorite');
+              return;
+            }
+            res.send('Favorite removed');
+          }
+        );
+      } else {
+        // Add the favorite
+        pool.query(
+          'INSERT INTO likes (user_id, game_id) VALUES (?, ?);',
+          [user_id, game_id],
+          (err, results) => {
+            if (err) {
+              console.error('Error adding favorite:', err);
+              res.status(500).send('Error adding favorite');
+              return;
+            }
+            res.send('Favorite added');
+          }
+        );
+      }
+    }
+  );
+});
+
+app.get('/check-login-status', (req, res) => {
+  if (req.session.user) {
+  res.json(true);
+  } else {
+  res.json(false);
+  }
+});
+
+const masters = ["nimishm2@illinois.edu", "rahulb3@illinois.edu"]
+
+app.get('/is-user-master', (req, res) => {
+  console.log(req.session.user);
+  console.log(req.session.user.email);
+  if (masters.includes(req.session.user.email)) {
+    res.json(true);
+  } else {
+    res.json(false);
+  }
+});
+
+// Update user endpoint
+app.put('/update-user', async (req, res) => {
+  const { id, email, password } = req.body;
+  const hashedPassword = await bcrypt.hash(password, 10);
+
+  pool.query(
+      'UPDATE users SET email = ?, password_hash = ? WHERE id = ?',
+      [email, hashedPassword, id],
+      (err, results) => {
+          if (err) {
+              console.error('Error updating user:', err);
+              res.status(500).send('Error updating user');
+              return;
+          }
+          res.send('User updated');
+      }
+  );
+});
+
+// Delete user endpoint
+app.delete('/delete-user/:id', (req, res) => {
+  const { id } = req.params;
+
+  pool.query(
+      'DELETE FROM users WHERE id = ?',
+      [id],
+      (err, results) => {
+        if (err) {
+            console.error('Error deleting user:', err);
+            res.status(500).send('Error deleting user');
+            return;
+        }
+        res.send('User deleted');
+    }
+);
+});
+
+
+// Login endpoint
+app.post('/login', (req, res) => {
+  const { email, password } = req.body;
+
+  pool.query('SELECT * FROM users WHERE email = ?', [email], async (err, results) => {
+    if (err || results.length === 0) {
+      res.status(401).send('Invalid email or password');
+      return;
+    }
+
+    const user = results[0];
+    const isPasswordValid = await bcrypt.compare(password, user.password_hash);
+
+    if (!isPasswordValid) {
+      res.status(401).send('Invalid email or password');
+      return;
+    }
+
+    req.session.user = { id: user.id, email: user.email };
+    res.send('Login successful');
+  });
+});
+
+
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log(`Server is running on http://localhost:${PORT}`);
+});
---
+++ public/bestValGamesSP.sql
@@ -0,0 +1,64 @@
+CREATE DEFINER=`root`@`%` PROCEDURE `bestVal`()
+BEGIN
+    DECLARE done BOOLEAN DEFAULT FALSE;
+    DECLARE curr_game VARCHAR(250);
+    DECLARE curr_critic INT;
+    DECLARE curr_price FLOAT;
+    DECLARE curr_upvote INT;
+    DECLARE curr_popularity VARCHAR(100);
+    DECLARE curr_isFave VARCHAR(30);
+    DECLARE g_cursor CURSOR FOR SELECT DISTINCT QueryName, MetaCritic, PriceFinal, RecommendationCount, '-'  
+								FROM GameFinder.Games NATURAL JOIN 
+                                (SELECT DISTINCT game_id,COUNT(game_id) AS cG FROM GameFinder.liked GROUP BY game_id HAVING cG>1) AS T 
+                                WHERE Metacritic >90 AND PriceFinal <15.00
+                                UNION
+                                SELECT DISTINCT QueryName, MetaCritic, PriceFinal, RecommendationCount, 'Site Favorite'  
+								FROM (SELECT * FROM GameFinder.Games WHERE QueryID IN (SELECT g_id FROM gf_fan_fave)) AS fanFave NATURAL JOIN 
+                                (SELECT DISTINCT game_id,COUNT(game_id) AS cG FROM GameFinder.likes GROUP BY game_id HAVING cG>1) AS T 
+                                WHERE fanFave.PriceFinal <15.00;
+                                
+    DECLARE CONTINUE HANDLER FOR NOT FOUND SET done = TRUE;
+    
+    DROP TABLE IF EXISTS bestValGames;
+    CREATE TABLE bestValGames(
+		game_name VARCHAR(250),
+        critic_score INT,
+        price FLOAT,
+        popularity VARCHAR(100),
+        is_fave VARCHAR(30)
+    );
+    
+    OPEN g_cursor;
+	read_loop: LOOP
+    FETCH g_cursor INTO curr_game,curr_critic,curr_price,curr_upvote,curr_isFave;
+    
+	IF done THEN
+      LEAVE read_loop;
+    END IF;
+    
+    IF curr_upvote <= 1000 THEN
+		SET curr_popularity = "Underrated";
+	ELSEIF curr_upvote > 1000 AND curr_upvote <= 5000 THEN
+		SET curr_popularity = "Popular";
+	ELSEIF curr_upvote > 5000 AND curr_upvote <= 10000 THEN
+		SET curr_popularity = "Fan Favourite";
+	ELSEIF curr_upvote > 10000 AND curr_upvote <= 50000 THEN
+		SET curr_popularity = "Masterpiece";
+	ELSE
+		SET curr_popularity = "Legendary";
+	END IF;
+    
+    INSERT IGNORE INTO bestValGames VALUE(curr_game,curr_critic,curr_price,curr_popularity, curr_isFave);
+    END LOOP;
+    
+    
+    CLOSE g_cursor;
+    
+    SELECT DISTINCT bestValGames.game_name, critic_score, price, popularity, 
+					is_fave, IFNULL(curr_playtime,0) AS play_time
+			FROM bestValGames LEFT JOIN 
+			(SELECT Name AS game_name, AVG(Hours) AS curr_playtime 
+			FROM Purchases GROUP BY game_name) AS temp 
+            ON (temp.game_name=bestValGames.game_name) ORDER BY price;
+    
+	END
